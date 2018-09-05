3ffb7b31cbbd3fa4e98f11a22fec7a6e9b37fc62
gaboadm <juan.lopez@digital-cupcake.com>
2018-09-05 16:23:07 -0400

    minor change constants TEST_USER_EMAIL

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -5,7 +5,7 @@ export const API_BOOKING_URL = '/bookings'
 
 export const TEST_ADMIN_EMAIL = 'testapis@tuten.cl';
 export const TEST_ADMIN_PASSWORD = '1234';
-export const TEST_USER_EMAIL = 'miguel@tuten.cl'
+export const TEST_USER_EMAIL = 'contacto@tuten.cl'
 
 export const KEY_TOKEN = 'api_token';
 export const KEY_USER_EMAIL = 'user_email';
